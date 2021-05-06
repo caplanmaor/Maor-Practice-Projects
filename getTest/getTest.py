2bc1f40a8d8b0a67bbeb4783617de8c5ce7bae57
Caplan Maor <caplanmaor@gmail.com>
2021-05-06 14:05:45 +0300

    getTest before the fix

---
+++ getTest/getTest.py
@@ -0,0 +1,13 @@
+#Create dictionary
+name_for_id = {
+	265: "Maor",
+	452: "Agam",
+	134: "Haim",
+}
+
+#Print name function
+def greeting(userid):
+	print("Hi %s !" % name_for_id[userid])
+
+#This is a bug, implement a default statement if the number is not in the dictionary
+greeting(266)
